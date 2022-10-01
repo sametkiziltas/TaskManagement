afbaa155a9991678851ccdaf1894ba0e24f16074
skiziltas <samet.kiziltas@hangikredi.com>
2022-10-01 18:38:40 +0300

    2

---
+++ TaskManagement.API/DataLayer/Entities/TMTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskManagement.API.DataLayer.Base;
+
+namespace TaskManagement.API.DataLayer.Entities
+{
+    public class TMTask: BaseEntity<Guid>
+    {
+        public DateTime CreatedDate { get; set; }
+        public string TaskDescription { get; set; }
+        public StatusOfTask StatusOfTask { get; set; }        
+        public TypeOfTask TypeOfTask { get; set; }        
+        public List<Comment> Comments{ get; set; }
+        public string AssignedTo{ get; set; }
+        public DateTime NextActionDate { get; set; }
+        
+    }
+    
+
+    public enum TypeOfTask
+    {
+        Story = 1,
+        Bug = 2,
+        Epic = 3
+    }
+
+    public enum StatusOfTask
+    {
+        Todo = 1,
+        InDevelopment = 2,
+        Test = 3,
+        Done = 4
+    }
+}
---
+++ TaskManagement.API/ServiceLayer/CommentService.cs
@@ -0,0 +1,21 @@
+﻿using TaskManagement.API.DataLayer.Repositories;
+
+namespace CommentManagement.API.ServiceLayer
+{
+
+    public interface ICommentService
+    {
+        
+    }
+    public class CommentService : ICommentService
+    {
+        private readonly ICommentRepository _commentRepository;
+        public CommentService(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+        
+    }
+
+    
+}
